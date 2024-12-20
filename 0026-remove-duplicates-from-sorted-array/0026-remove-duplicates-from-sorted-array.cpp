c46f794a06e87bea117154b500978bd4e0cd0503
Shrayash awasthi <awasthishrayashofc@gmail.com>
2024-12-20 21:33:02 +0530

    Update 0026-remove-duplicates-from-sorted-array.cpp

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         //Brute force approch is taking an empty set and try to put the elements of array in it
-        int i=0;
+        int i=0; // optimal approch is to take a pointer at first position and find the element not equal to it , then put that
+        // element (found one) to the second index and increase the pointer by 1 position
         for(int j=0;j<nums.size();j++){
             if(nums[i]!=nums[j]){
                 nums[i+1]=nums[j];
