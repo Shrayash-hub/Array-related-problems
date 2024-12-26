6011b5e939fca68834901f54caa6fb96b961af60
Shrayash awasthi <awasthishrayashofc@gmail.com>
2024-12-27 03:29:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+
+        // your code here
+        int el = -1;
+        int count=0;
+        // Moore’s Voting Algorithm:
+        for(int i=0;i<arr.size();i++){
+            if(count==0){
+                el = arr[i];
+                count=1;
+            }
+            else if(arr[i]==el){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int count1=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==el){
+                count1++;
+            }
+        }
+        if(count1>arr.size()/2){
+            return el;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n;
+        vector<int> a, b;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.majorityElement(a) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
