a20bc16ad08341be84f47ce9f8a4e8347ca1f1ec
Shrayash awasthi <awasthishrayashofc@gmail.com>
2025-08-13 10:15:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,49 @@
+class Solution {
+  public:
+    int FindFirst(vector<int>& arr, int x){
+        int low = 0;
+        int high = arr.size()-1;
+        int first = -1;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(arr[mid]==x){  // for first occurence , we have to search on the left(starting side) of the array
+                first = mid;
+                high = mid-1; // so we have to move left side , even if we found the target element, to find the lowest index with the target value
+            }
+            else if(arr[mid]<x){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return first;
+    }
+    int FindSecond(vector<int>& arr, int x){
+        int low = 0;
+        int high = arr.size()-1;
+        int second = -1;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(arr[mid]==x){     // for second occurence , we have to search on the righ(ending side) of the array
+                second = mid;
+                low = mid+1;   // so we have to move right side , even if we found the target element, to find the highest index with the target value
+            }
+            else if(arr[mid]<x){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return second;
+    }
+    int countFreq(vector<int>& arr, int target) {
+        // code here
+        int first = FindFirst(arr,target);
+        if(first==-1) return 0;
+        int Last = FindSecond(arr,target);
+        return Last-first+1;
+        
+    }
+};
