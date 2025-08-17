ba693266ef154f966df95d0479e0b7c7086d8376
Shrayash awasthi <awasthishrayashofc@gmail.com>
2025-08-17 13:26:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -1,78 +1,37 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
 class Solution {
   public:
     int findKRotation(vector<int> &arr) {
-        int low = 0, high = arr.size() - 1;
-    int ans = INT_MAX;
-    int index = -1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        //search space is already sorted
-        //then arr[low] will always be
-        //the minimum in that search space:
-        if (arr[low] <= arr[high]) {
-            if (arr[low] < ans) {
-                index = low;
-                ans = arr[low];
+        // Code Here
+        int low = 0;
+        int high = arr.size()-1;
+        int index;
+        int ans= INT_MAX;
+        // no. of rotation = index of minimum element
+        while(low<=high){
+            if(arr[low]<=arr[high]){
+                if(arr[low]<ans){   // check that arr[low] is minimum or not
+                    index = low;
+                    ans = arr[low];
+                }
+                break;
             }
-            break;
-        }
-
-        //if left part is sorted:
-        if (arr[low] <= arr[mid]) {
-            // keep the minimum:
-            if (arr[low] < ans) {
-                index = low;
-                ans = arr[low];
+            int mid = (low+high)/2;
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<ans){
+                    ans = arr[low];
+                    index = low;
+                }
+                low = mid+1;
             }
-
-            // Eliminate left half:
-            low = mid + 1;
-        }
-        else { //if right part is sorted:
-
-            // keep the minimum:
-            if (arr[mid] < ans) {
-                index = mid;
-                ans = arr[mid];
+            else{
+                if(arr[mid]<ans){
+                    ans = arr[mid];
+                    index = mid;
+                }
+                high = mid-1;
+                }
             }
-
-            // Eliminate right half:
-            high = mid - 1;
+            return index;
         }
-    }
-    return index;
-    }
+    
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input1;
-        getline(cin, input1);
-        stringstream ss1(input1);
-        int number1;
-        while (ss1 >> number1) {
-            arr.push_back(number1);
-        }
-        Solution ob;
-        int res = ob.findKRotation(arr);
-        cout << res << endl;
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends
